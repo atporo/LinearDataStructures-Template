0bf1b2cd0907bfe4ceddc13939276c4e80a8f24d
Roland Pogner <93944508+atporo@users.noreply.github.com>
2024-09-24 18:51:44 +0200

    Initial

---
+++ Generics/DemoGenerics/DemoGenerics/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Point<int> intPoint = new Point<int>();
+            Point<double> doublePoint = new Point<double>();
+
+            Console.WriteLine($"TYP von X: {doublePoint.x.GetType().Name}"); ;
+            
+        }
+    }
+}
---
+++ Generics/DemoGenerics/DemoGenerics/point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable disable
+
+namespace DemoGenerics
+{
+    internal class Point<T> // Klasse vom Typ point generisch
+    {
+        public T x {  get; set; }
+
+        public T y { get; set; }
+    }
+}
