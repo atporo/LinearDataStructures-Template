cb16060ae0a22366e95890b1c91b3eca4b24c0ed
Roland Pogner <93944508+atporo@users.noreply.github.com>
2024-09-24 20:04:49 +0200

    add initial Stack

---
+++ LinearDataStructures.Logic/Stack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearDataStructures.Logic
+{
+    internal class Stack<T> : IStack<T>
+    {
+
+        #region fields
+        private Element<T>? _first = null;
+        #endregion fields
+
+        public bool IsEmpty => throw new NotImplementedException();
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
